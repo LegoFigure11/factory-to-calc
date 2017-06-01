8da5e906cba597a8edcba7d80ddb945e3290025b
LegoFigure11 <legofigurefb@gmail.com>
2017-06-01 16:10:47 +1000

    Update script.js

--- script.js
+++ script.js
@@ -133,10 +133,11 @@ var savecustom = function()
 		}
 		else var spev = ln10p1.concat(string.substring(string.indexOf('"spe":')+6, string.indexOf('},"nature"')), lnbr)
 		
-		var movesall = string.substring(string.indexOf('"moves":[[')+11, string.lastIndexOf(']]')-3)
+		var movesall = string.substring(string.indexOf('"moves":[[')+11, string.lastIndexOf(']]'))
 		var move1 = string.substring(string.indexOf('"moves":[[')+11, string.indexOf('"],['))
 		// var m2p1 = string.substring(string.indexOf('"moves":[[')+10)
-		var move2 = string.substring(string.indexOf(move1) + move1.length + 4, string.indexOf('"],['))
+		var m2p1 = movesall.indexOf(move1) + move1.length() + 4
+		var move2 = string.substring(string.indexOf(move1) + move1.length() + 4, string.indexOf('"],['))
 				
 		/*
 		if (string.substring(string.indexOf('"hp":')+5, string.indexOf(',"atk"')) == 31)
