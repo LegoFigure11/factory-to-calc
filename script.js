67c8f711a672ce0d33d2c5055a136dd56a833f46
LegoFigure11 <legofigurefb@gmail.com>
2017-06-01 16:06:01 +1000

    Update script.js

--- script.js
+++ script.js
@@ -67,93 +67,7 @@ var savecustom = function()
 	var nature = string.substring(string.indexOf('"nature":"')+10, string.indexOf('","moves'));
 	var ability = string.substring(string.indexOf('"ability":"')+11, string.indexOf('","evs":'));
 
-	/* if(lines[0].indexOf('(M)') != -1)
-	{
-		lines[0] = lines[0].substring(0, lines[0].indexOf('(M)') - 1) + 
-		lines[0].substring(lines[0].indexOf('(M)') + 3, lines[0].length);
-	}
-	else if(lines[0].indexOf('(F)') != -1)
-	{
-		lines[0] = lines[0].substring(0, lines[0].indexOf('(F)')) + 
-		lines[0].substring(lines[0].indexOf('(F)') + 3, lines[0].length);
-	}
-	if(lines[0].indexOf('(') != -1)
-	{
-		firstParenth = lines[0].lastIndexOf('(');
-		lastParenth = lines[0].lastIndexOf(')');
-		species = lines[0].substring(firstParenth + 1, lastParenth).trim();
-	}
-	else
-		species = lines[0].split('@')[0].trim(); //species is always first
-	for(var i = 0; i < showdownFormes.length; ++i)
-	{
-		if(species == showdownFormes[i][0])
-			species = showdownFormes[i][1]
-	}
-	
-	if(lines[0].indexOf('@') != -1)
-		item = lines[0].substring(lines[0].indexOf('@')+1).trim(); //item is always after @
-	ability = lines[1].substring(lines[1].indexOf(' ')+1).trim(); //ability is always second
-	if(lines.length > 2){
-		for(var i = 2; i < lines.length; ++i){
-			if(lines[i].indexOf("Level") != -1){
-				level = lines[2].split(' ')[1].trim(); //level is sometimes third but uh not always
-			}
-			if(lines[i].indexOf("EVs") != -1) //if EVs are in this line
-			{
-				evList = lines[i].split(':')[1].split('/'); //splitting it into a list of " # Stat "
-				for(var j = 0; j < evList.length; ++j){
-					evList[j] = evList[j].trim();
-					evListElements = evList[j].split(' ');
-					if(evListElements[1] == "HP")
-						EVs[0] = parseInt(evListElements[0])
-					else if(evListElements[1] == "Atk")
-						EVs[1] = parseInt(evListElements[0])
-					else if(evListElements[1] == "Def")
-						EVs[2] = parseInt(evListElements[0])
-					else if(evListElements[1] == "SpA")
-						EVs[3] = parseInt(evListElements[0])
-					else if(evListElements[1] == "SpD")
-						EVs[4] = parseInt(evListElements[0])
-					else if(evListElements[1] == "Spe")
-						EVs[5] = parseInt(evListElements[0])
-				}
-
-			}
-			if(lines[i].indexOf("IVs") != -1) //if IVs are in this line
-			{
-				var ln12p1 = '      "ivs": { \n'
-				var ln11v2 = '      }, \n'
-				ivList = lines[i].split(':')[1].split('/'); //splitting it into a list of " # Stat "
-				for(var j = 0; j < ivList.length; ++j){
-					ivList[j] = ivList[j].trim();
-					ivListElements = ivList[j].split(' ');
-					if(ivListElements[1] == "HP")
-						IVs[0] = parseInt(ivListElements[0])
-					else if(ivListElements[1] == "Atk")
-						IVs[1] = parseInt(ivListElements[0])
-					else if(ivListElements[1] == "Def")
-						IVs[2] = parseInt(ivListElements[0])
-					else if(ivListElements[1] == "SpA")
-						IVs[3] = parseInt(ivListElements[0])
-					else if(ivListElements[1] == "SpD")
-						IVs[4] = parseInt(ivListElements[0])
-					else if(ivListElements[1] == "Spe")
-						IVs[5] = parseInt(ivListElements[0])
-				}
-
-
-			}    
-			if(lines[i].indexOf("Nature") != -1) //if nature is in this line
-			{
-				nature = lines[i].split(' ')[0].trim()
-			}
-			if(lines[i].indexOf("- ") != -1){ //if there is a move in this line
-				var nextMove = lines[i].substring(lines[i].indexOf(' ') + 1).trim()
-				 nextMove = nextMove.replace('[', '')
-				 nextMove = nextMove.replace(']', '')
-				 movarray.push(nextMove)
-			}
+	/*
 			var movlist = movarray.toString()
 			var move1 = movlist.split(",")[0]
 			var move2 = movlist.split(",")[1]
@@ -222,7 +136,7 @@ var savecustom = function()
 		var movesall = string.substring(string.indexOf('"moves":[[')+11, string.lastIndexOf(']]')-3)
 		var move1 = string.substring(string.indexOf('"moves":[[')+11, string.indexOf('"],['))
 		// var m2p1 = string.substring(string.indexOf('"moves":[[')+10)
-		var move2 = string.substring(string.indexOf(move1) + move1.length + 4, string.indexOf('"],[')).concat(lnen, lnmv)
+		var move2 = string.substring(string.indexOf(move1) + move1.length + 4, string.indexOf('"],['))
 				
 		/*
 		if (string.substring(string.indexOf('"hp":')+5, string.indexOf(',"atk"')) == 31)
@@ -262,7 +176,7 @@ var savecustom = function()
 		else var spiv = ln10p1.concat(IVs[5], lnbr)
 		*/
 		
-	var res = ln1p1.concat(species, ln1p2, ln2p1, ln2p2, ln2p3, ln3, ln4, hpev, atev, deev, saev, sdev, spev, ln11, ln12p1, ln19p1, nature, lnen, ln20p1, ability, lnen, ln21p1, item, lnen, ln22, lnmv, movesall)	
+	var res = ln1p1.concat(species, ln1p2, ln2p1, ln2p2, ln2p3, ln3, ln4, hpev, atev, deev, saev, sdev, spev, ln11, ln12p1, ln19p1, nature, lnen, ln20p1, ability, lnen, ln21p1, item, lnen, ln22, lnmv, move1, lnen, lnmv, move2, lnen, lnmv, movesall)	
 	//var res = ln1p1.concat(species, ln1p2, ln2p1, ln2p2, ln2p3, ln3, ln4, hpev, atev, deev, saev, sdev, spev, ln11, ln12p1, hpiv, ativ, deiv, saiv, sdiv, spiv, ln11v2, ln19p1, nature, lnen, ln20p1, ability, lnen, ln21p1, item, lnen, ln22, lnmv, move1, lnen, lnmv, move2, lnen, lnmv, move3, lnen, lnmv, move4, lnenl, ln3l, ln2l, lnl);
     document.getElementById("Output").innerHTML = res
 
